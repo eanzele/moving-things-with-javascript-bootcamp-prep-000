60d3e15eb5b96788c263eefcc6a158041a143503
Emmanuel Anzele <eanzele@gmail.com>
2018-12-30 22:25:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var dodger = document.getElementById('dodger');
 dodger.style.backgroundColor = "#FF69B4";
 dodger.style.bottom = '100px';
 document.addEventListener('keydown', function(e) {
-  console.log(e.which)
-})
+  console.log(e.which);
+  });
